2e14e84000aee50dd2f51814e9388e46e77119eb
Simon 42 <Monkeymandeveloper@protonmail.com>
2024-04-30 16:49:51 +0200

    ex01 init

---
+++ ex01/include/ClapTrap.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 09:54:18 by svolodin          #+#    #+#             */
+/*   Updated: 2024/04/30 16:13:52 by svolodin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _CLAPTRAP_HPP_
+# define _CLAPTRAP_HPP_
+
+#include "Colors.h"
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+  private:
+  
+    std::string _name;
+    int _hitPoints;
+    int _energyPoints;
+    int _attackDamage;
+    
+  public:
+
+    ClapTrap(const std::string& name);
+    ClapTrap(const ClapTrap& src);
+    ~ClapTrap();
+
+    ClapTrap& operator=(const ClapTrap& src);
+
+    void  setName(std::string name);
+
+    /* Actions (inside `ClapTrap.Action.cpp`) */
+    void  attack(const std::string& target);
+    void  takeDamage(unsigned int amount);
+    void  beRepaired(unsigned int amount);
+
+};
+
+#endif
---
+++ ex01/src/ClapTrap.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svolodin <svolodin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 09:54:47 by svolodin          #+#    #+#             */
+/*   Updated: 2024/04/30 16:33:33 by svolodin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(const std::string& name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+  std::cout << KGRN << "ClapTrap named " << BOLD_A << _name << RST;
+  std::cout << KGRN << " was born." << RST << "\n";
+  
+  return ;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& src)
+{
+  std::cout << KGRN << "ClapTrap copy constructor was called on " << BOLD_A << src._name << RST << "\n";
+  *this = src;
+
+  return ;
+}
+
+ClapTrap::~ClapTrap()
+{
+  std::cout << KRED << "ClapTrap named " << BOLD_A << _name << RST;
+  std::cout << KRED << " was destroyed." << RST << "\n";
+
+  return ;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& src)
+{
+  std::cout << KGRN << "ClapTrap copy assignment constructor was called on " << BOLD_A << src._name << RST << "\n";
+  if (this != &src) {
+    _name = src._name;
+    _hitPoints = src._hitPoints;
+    _energyPoints = src._energyPoints;
+    _attackDamage = src._attackDamage;
+  }
+
+  return *this;
+}
+
+void  ClapTrap::setName(std::string name)
+{
+  std::cout << KGRN << BOLD_A << _name << RST;
+  std::cout << KGRN << " was renamed to " << BOLD_A << name << RST << "\n";
+
+  _name = name;
+
+  return ;
+}
